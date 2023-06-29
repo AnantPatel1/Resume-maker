11ce0f196ba9859a2bde7f678c0d347909252af8
Anant <anantpatel07012003@gmail.com>
2023-06-29 20:27:13 +0530

    build

--- src/Navigation-Page/BuildResume/BuildResume.js
+++ src/Navigation-Page/BuildResume/BuildResume.js
@@ -1,5 +1,51 @@
-import React from "react";
-
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import Template from "../../Assets/Resume Theme - 1.png";
+import PersonalInfoForm from "./PersonalDetails";
+import classes from "./BuildResume.module.css";
 export default function BuildResume() {
-  return <div>BuildResume</div>;
+  const [value, setValue] = React.useState("1");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.item}>
+        {" "}
+        <Box sx={{ width: "100%", typography: "body1", marginTop: "30px" }}>
+          <TabContext value={value}>
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+              <TabList
+                onChange={handleChange}
+                aria-label="lab API tabs example"
+              >
+                <Tab label="Details" value="1" />
+                <Tab label="Item Two" value="2" />
+                <Tab label="Item Three" value="3" />
+                <Tab label="Item Three" value="4" />
+                <Tab label="Item Three" value="5" />
+              </TabList>
+            </Box>
+            <TabPanel value="1">
+              <PersonalInfoForm />
+            </TabPanel>
+            <TabPanel value="2">Item Two</TabPanel>
+            <TabPanel value="3">Item Three</TabPanel>
+            <TabPanel value="4">Item One</TabPanel>
+            <TabPanel value="5">Item One</TabPanel>
+          </TabContext>
+             
+        </Box>
+      </div>
+      <div className={`${classes.item} ${classes.imageItem} `}>
+        <img className={classes.template1} src={Template} alt="Template1" />
+      </div>
+    </div>
+  );
 }
---
+++ src/Navigation-Page/BuildResume/PersonalDetails.js
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import { TextField } from "@mui/material";
+import classes from "./PersonalDetails.module.css";
+
+const PersonalDetails = () => {
+  const [file, setFile] = useState(null);
+  const [name, setName] = useState("");
+  const [summary, setSummary] = useState("");
+  const [workProfile, setWorkProfile] = useState("");
+  const [address, setAddress] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [emailId, setEmailId] = useState("");
+
+  const handleUpload = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    console.log(name);
+    console.log(value);
+    switch (name) {
+      case "name":
+        setName(value);
+        break;
+      case "summary":
+        setSummary(value);
+        break;
+      case "workProfile":
+        setWorkProfile(value);
+        break;
+      case "address":
+        setAddress(value);
+        break;
+      case "phoneNumber":
+        setPhoneNumber(value);
+        break;
+      case "emailId":
+        setEmailId(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div className={classes.container}>
+      <h2>Personal Details</h2>
+      <input
+        type="file"
+        accept="image/jpeg, image/png"
+        className={classes.inputFile}
+        onChange={handleUpload}
+      />
+      <TextField
+        label="Your Name"
+        name="name"
+        value={name}
+        onChange={handleChange}
+        className={classes.textInput}
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        label="Your Summary"
+        name="summary"
+        value={summary}
+        onChange={handleChange}
+        className={classes.textInput}
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        label="Work Profile"
+        name="workProfile"
+        value={workProfile}
+        onChange={handleChange}
+        className={classes.textInput}
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        label="Address"
+        name="address"
+        value={address}
+        onChange={handleChange}
+        className={classes.textInput}
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        label="Phone Number"
+        name="phoneNumber"
+        value={phoneNumber}
+        onChange={handleChange}
+        className={classes.textInput}
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        label="Email ID"
+        name="emailId"
+        value={emailId}
+        onChange={handleChange}
+        className={classes.textInput}
+        margin="normal"
+        fullWidth
+      />
+    </div>
+  );
+};
+
+export default PersonalDetails;
