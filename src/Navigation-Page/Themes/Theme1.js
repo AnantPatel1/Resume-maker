89d307f359c34826026c9606ca7f75287efecdc2
Anant <anantpatel07012003@gmail.com>
2023-07-11 20:40:11 +0530

    education

--- src/Navigation-Page/Themes/Theme1.js
+++ src/Navigation-Page/Themes/Theme1.js
@@ -7,6 +7,7 @@ export default function Theme1() {
   const { themeData, ComponentRef } = user;
   const { name, address, email, profile, phone, skill } =
     themeData.personalData;
+  const { educationTitles, educationDesc } = themeData.educationData;
   // console.log(name);
   return (
     <div className={styles.Theme1} ref={ComponentRef}>
@@ -22,6 +23,9 @@ export default function Theme1() {
           <p className={styles.profile}>{profile}</p>
         </header>
       </section>
+      {
+        // Skills
+      }
 
       <section className={styles.skills}>
         <header>Technical skills</header>
@@ -33,6 +37,34 @@ export default function Theme1() {
               </li>
             ))}
           </ul>
+        </div>
+      </section>
+      {
+        // Education
+        // Object.entries() returns an array whose elements are arrays corresponding to the enumerable string-keyed property key-value pairs found directly upon object.
+        // The code is a React component that renders a list of education details. The component takes two dictionaries as input: educationTitles and educationDesc. The educationTitles dictionary maps an index to a title, such as "Bachelor of Science in Computer Science". The educationDesc dictionary maps an index to a description, such as "Stanford University".
+        // The code first uses the Object.entries() method to iterate over the educationTitles dictionary. For each entry, the code creates a Box component with the title as the text content. The Box component also has a class name of subBox.
+        // Next, the code checks if the educationDesc dictionary has an entry for the current index. If it does, the code uses the Object.entries() method to iterate over the educationDesc dictionary and creates a list of li elements for each description. The li elements contain the descriptions, which are split by commas.
+      }
+      <section className={styles.education}>
+        <header>Education</header>
+        <div className={styles["basic-set"]}>
+          {Object.entries(educationTitles).map((item, key) => {
+            return (
+              <div key={key} className={styles.subBox}>
+                <p className={styles["sub-title"]}>{item[1]}</p>
+                <div className={styles["sub-details"]}>
+                  {Object.entries(educationDesc)[key] === undefined
+                    ? null
+                    : Object.entries(educationDesc)
+                        [key][1].split(",")
+                        .map((element, index) => {
+                          return <li key={index}>{element}</li>;
+                        })}
+                </div>
+              </div>
+            );
+          })}
         </div>
       </section>
     </div>
